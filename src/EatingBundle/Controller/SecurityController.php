b6d671acdfba7f76c403ee9ecf909f2f308f4ec3
Kate Balan <ket11141@gmail.com>
2017-11-22 23:07:05 +0200

    registration

--- src/EatingBundle/Controller/SecurityController.php
+++ src/EatingBundle/Controller/SecurityController.php
@@ -2,10 +2,12 @@
 
 namespace EatingBundle\Controller;
 
+use EatingBundle\Entity\User;
 use EatingBundle\Form\LoginFormType;
 use EatingBundle\Form\UserRegistrationFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
@@ -45,11 +47,60 @@ class SecurityController extends Controller
     /**
      * @Route("/register", name="user_register")
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createForm(UserRegistrationFormType::class);
 
-        return $this->render('@Eating/security/login.html.twig', [
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+
+            $weight = $user->getWeight();
+            $energy_exchange = $user->getEnergyExchange();
+            $daily_kkal = 0;
+
+            if($user->getAge() <= 30 && $user->getGender() == false) {
+                $daily_kkal = (0.0621 * $weight + 2.0357) * 240 * $energy_exchange;
+            }
+            elseif($user->getAge() > 30 && $user->getAge() <= 60 && $user->getGender() == false) {
+                $daily_kkal = (0.0342 * $weight + 3.5377) * 240 * $energy_exchange;
+            }
+            elseif($user->getAge() > 60 && $user->getGender() == false) {
+                $daily_kkal = (0.0377 * $weight + 2.7545) * 240 * $energy_exchange;
+            }
+            elseif($user->getAge() <= 30 && $user->getGender() == true) {
+                $daily_kkal = (0.0630 * $weight + 2.8957) * 240 * $energy_exchange;
+            }
+            elseif($user->getAge() > 30 && $user->getGender() == true) {
+                $daily_kkal = (0.0491 * $weight + 2.4587) * 240 * $energy_exchange;
+            }
+            else {
+                $user->setDailyKkal(0);
+            }
+
+            $user->setDailyKkal(round($daily_kkal));
+            $daily_parts = round($daily_kkal / 6, 2, PHP_ROUND_HALF_UP);
+
+            $daily_fats = round($daily_parts / 9, 2, PHP_ROUND_HALF_UP);
+            $daily_proteins = round($daily_parts / 4, 2, PHP_ROUND_HALF_UP);
+            $daily_carbohydrates = $daily_parts;
+
+            $user->setDailyFats($daily_fats);
+            $user->setDailyProteins($daily_proteins);
+            $user->setDailyCarbohydrates($daily_carbohydrates);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Welcome '.$user->getFirstName().' '.$user->getSecondName());
+
+            return $this->redirectToRoute('user_show', ['userId' => $user->getId()]);
+        }
+
+        return $this->render('@Eating/security/register.html.twig', [
             'form' => $form->createView()
         ]);
     }
--- src/EatingBundle/Form/UserRegistrationFormType.php
+++ src/EatingBundle/Form/UserRegistrationFormType.php
@@ -5,6 +5,7 @@ namespace EatingBundle\Form;
 use EatingBundle\Entity\User;
 use Symfony\Component\DependencyInjection\Compiler\RepeatedPass;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -19,6 +20,41 @@ class UserRegistrationFormType extends AbstractType
             ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class
+            ])
+            ->add('firstName')
+            ->add('secondName')
+            ->add('age', null, [
+                'attr' => ['class' => 'ea-form__field'],
+                'label_attr' => ['class' => 'ea-form__label']
+            ])
+            ->add('gender', ChoiceType::class, [
+                'attr' => ['class' => 'ea-form__field'],
+                'label_attr' => ['class' => 'ea-form__label'],
+                'choices' => [
+                    'Male' => true,
+                    'Female' => false,
+                ]
+            ])
+            ->add('phone', null, [
+                'attr' => ['class' => 'ea-form__field'],
+                'label_attr' => ['class' => 'ea-form__label']
+            ])
+            ->add('weight', null, [
+                'attr' => ['class' => 'ea-form__field'],
+                'label_attr' => ['class' => 'ea-form__label']
+            ])
+            ->add('height', null, [
+                'attr' => ['class' => 'ea-form__field'],
+                'label_attr' => ['class' => 'ea-form__label']
+            ])
+            ->add('energyExchange', ChoiceType::class, [
+                'attr' => ['class' => 'ea-form__field'],
+                'label_attr' => ['class' => 'ea-form__label'],
+                'choices' => [
+                    'Low activity (you are passive)' => 1.1,
+                    'Moderate activity (work is sitting, but the office has to run, and in addition, two or three times a week you find time for sports).' => 1.3,
+                    'High activity (your work is a constant movement)' => 1.5
+                ]
             ]);
     }
 

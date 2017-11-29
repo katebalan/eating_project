8c6949486482889c5499d84028e47f7ab9919c3a
Kate Balan <ket11141@gmail.com>
2017-11-29 20:51:13 +0200

    code style

--- src/EatingBundle/Controller/RecommendationController.php
+++ src/EatingBundle/Controller/RecommendationController.php
@@ -8,60 +8,48 @@ class RecommendationController extends Controller
     /**
      * @Route("/recommendation", name="recommendation_list")
      */
-   /* public function showAction()
-    {
-        return $this->render('EatingBundle:Recommendation:list.html.twig');
-    }
-
-  /*  public function recommendationListAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $products = $em->getRepository('EatingBundle:Products')->findAllOrderedByDescActive();
-
-        return $this->render('EatingBundle:Products:list.html.twig', [
-            'products' => $products
-        ]);
-    }*/
-
-
     public function productsListAction()
     {
         $em = $this->getDoctrine()->getManager();
         $products = $em->getRepository('EatingBundle:Products')->findAllOrderedByDescActive();
-        $pr=array();
-        $user= $this->getUser();
-        $proteins=$user->getDailyProteins()-$user->getCurrentProteins();
-        $fats=$user->getDailyFats()-$user->getCurrentFats();
-        $carbohydrates=$user->getDailyCarbohydrates()-$user->getCurrentCarbohydrates();
-        $sum=$proteins+ $fats+$carbohydrates;
-        $proteins=100*$proteins/$sum;
-        $fats=100*$fats/$sum;
-        $carbohydrates=100*$carbohydrates/$sum;
+        $pr = array();
+
+        $user = $this->getUser();
+
+        $proteins = $user->getDailyProteins() - $user->getCurrentProteins();
+        $fats = $user->getDailyFats() - $user->getCurrentFats();
+        $carbohydrates = $user->getDailyCarbohydrates() - $user->getCurrentCarbohydrates();
+
+        $sum = $proteins + $fats + $carbohydrates;
+        $proteins = 100 * $proteins / $sum;
+        $fats = 100 * $fats / $sum;
+        $carbohydrates = 100 * $carbohydrates / $sum;
+
         foreach ($products as $i => $value) {
-            $Dproteins=$products[$i]->getProteinsPer100gr()-$proteins;
-            $Dfats=$products[$i]->getFatsPer100gr()-$fats;
-            $Dcarbohydrates=$products[$i]->getCarbohydratesPer100gr()-$carbohydrates;
-           if( $Dproteins*$Dproteins+$Dfats*$Dfats+$Dcarbohydrates*$Dcarbohydrates<3000) $pr[]=$products[$i];
+            $Dproteins = $products[$i]->getProteinsPer100gr() - $proteins;
+            $Dfats = $products[$i]->getFatsPer100gr() - $fats;
+            $Dcarbohydrates = $products[$i]->getCarbohydratesPer100gr() - $carbohydrates;
+            if( $Dproteins * $Dproteins + $Dfats * $Dfats + $Dcarbohydrates * $Dcarbohydrates < 3000)
+                $pr[] = $products[$i];
         }
+
         usort($pr, function($a, $b) {
-     /*       if ($a == $b)
+            /* if ($a == $b)
             {
                 return 0;
             }
-            else */if ($a->getRating() > $b->getRating())
-            {
+            else */
+            if ($a->getRating() > $b->getRating()) {
                 return -1;
             }
             else {
                 return 1;
             }
+        });
 
-        }
-        );
         return $this->render('EatingBundle:Recommendation:list.html.twig', [
             'products' => $pr
         ]);
-
     }
 
 

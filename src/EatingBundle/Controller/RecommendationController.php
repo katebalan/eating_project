015954c500c33bc9a0a1f2e0a81ebb0b3aa79d7a
Kate Balan <ket11141@gmail.com>
2017-11-30 11:25:48 +0200

    sveta: git pull

--- src/EatingBundle/Controller/RecommendationController.php
+++ src/EatingBundle/Controller/RecommendationController.php
@@ -28,15 +28,20 @@ class RecommendationController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $products = $em->getRepository('EatingBundle:Products')->findAllOrderedByDescActive();
-        $pr=array();
-        $user= $this->getUser();
+
+        $pr = array();
+
+        $user = $this->getUser();
+
         $proteins=$user->getDailyProteins()-$user->getCurrentProteins();
         $fats=$user->getDailyFats()-$user->getCurrentFats();
         $carbohydrates=$user->getDailyCarbohydrates()-$user->getCurrentCarbohydrates();
+
         $sum=$proteins+ $fats+$carbohydrates;
         $proteins=100*$proteins/$sum;
         $fats=100*$fats/$sum;
         $carbohydrates=100*$carbohydrates/$sum;
+
         foreach ($products as $i => $value) {
             $Dproteins=$products[$i]->getProteinsPer100gr()-$proteins;
             $Dfats=$products[$i]->getFatsPer100gr()-$fats;
